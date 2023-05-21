2ca94d010aed453778cc39752cecad536c107ab2
junpei0223 <junpei0223@gmail.com>
2023-05-21 23:51:14 +0900

    context.WithValueのkeyは空構造体で定義するのが一般的みたいなので修正 https://zenn.dev/hsaki/books/golang-context/viewer/appliedvalue

--- auth/auth.go
+++ auth/auth.go
@@ -5,18 +5,14 @@ import (
 	"errors"
 )
 
-type ctxKey int
-
-const (
-	authToken ctxKey = iota
-)
+type ctxKey struct{}
 
 func SetAuthToken(ctx context.Context, token string) context.Context {
-	return context.WithValue(ctx, authToken, token)
+	return context.WithValue(ctx, ctxKey{}, token)
 }
 
 func getAuthToken(ctx context.Context) (string, error) {
-	if token, ok := ctx.Value(authToken).(string); ok {
+	if token, ok := ctx.Value(ctxKey{}).(string); ok {
 		return token, nil
 	}
 	return "", errors.New("cannot find auth token")
--- session/session.go
+++ session/session.go
@@ -2,21 +2,17 @@ package session
 
 import "context"
 
-type ctxKey int
-
-const (
-	sessionID ctxKey = iota
-)
+type ctxKey struct{}
 
 var sequence int = 1
 
 func SetSessionID(ctx context.Context) context.Context {
-	idCtx := context.WithValue(ctx, sessionID, sequence)
+	idCtx := context.WithValue(ctx, ctxKey{}, sequence)
 	sequence += 1
 	return idCtx
 }
 
 func GetSessionID(ctx context.Context) int {
-	id := ctx.Value(sessionID).(int)
+	id := ctx.Value(ctxKey{}).(int)
 	return id
 }
